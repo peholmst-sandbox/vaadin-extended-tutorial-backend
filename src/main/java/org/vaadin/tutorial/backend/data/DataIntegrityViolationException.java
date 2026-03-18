515e53963ab28f8241b816cf3838203cb44bc60a
Petter Holmstrom <petter@vaadin.com>
2026-03-18 15:02:28 +0200

    Add constructors

--- src/main/java/org/vaadin/tutorial/backend/data/DataIntegrityViolationException.java
+++ src/main/java/org/vaadin/tutorial/backend/data/DataIntegrityViolationException.java
@@ -1,4 +1,11 @@
 package org.vaadin.tutorial.backend.data;
 
 public class DataIntegrityViolationException extends RuntimeException {
+
+    public DataIntegrityViolationException() {
+    }
+
+    public DataIntegrityViolationException(String message) {
+        super(message);
+    }
 }
--- src/main/java/org/vaadin/tutorial/backend/data/OptimisticLockingFailureException.java
+++ src/main/java/org/vaadin/tutorial/backend/data/OptimisticLockingFailureException.java
@@ -1,4 +1,11 @@
 package org.vaadin.tutorial.backend.data;
 
 public class OptimisticLockingFailureException extends RuntimeException {
+
+    public OptimisticLockingFailureException() {
+    }
+
+    public OptimisticLockingFailureException(String message) {
+        super(message);
+    }
 }
