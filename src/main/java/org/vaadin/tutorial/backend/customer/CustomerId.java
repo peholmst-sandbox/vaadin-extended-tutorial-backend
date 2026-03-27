2630c7f570ad34c0c5bf4273f687abba13c796f7
Petter Holmstrom <petter@vaadin.com>
2026-03-27 16:25:04 +0200

    Update toString()

--- src/main/java/org/vaadin/tutorial/backend/customer/CustomerId.java
+++ src/main/java/org/vaadin/tutorial/backend/customer/CustomerId.java
@@ -3,4 +3,9 @@ package org.vaadin.tutorial.backend.customer;
 import java.io.Serializable;
 
 public record CustomerId(long id) implements Serializable {
+
+    @Override
+    public String toString() {
+        return Long.toString(id);
+    }
 }
--- src/main/java/org/vaadin/tutorial/backend/order/OrderId.java
+++ src/main/java/org/vaadin/tutorial/backend/order/OrderId.java
@@ -3,4 +3,9 @@ package org.vaadin.tutorial.backend.order;
 import java.io.Serializable;
 
 public record OrderId(long id) implements Serializable {
+
+    @Override
+    public String toString() {
+        return Long.toString(id);
+    }
 }
